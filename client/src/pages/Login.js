a9f62e64f671e238b860921dceed897c769beb68
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-04-13 20:27:31 -0700

    Login page design implementation

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,47 +1,99 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+// import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
+// import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../utils/auth';
+// import Auth from '../utils/auth';
 
-const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+// MATERIALS
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+///////////////////
+
+const Login = () => {
+// const Login = (props) => {
+  // const [formState, setFormState] = useState({ email: '', password: '' });
+  // const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
 
   // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-    try {
-      const { data } = await login({
-        variables: { ...formState },
-      });
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   console.log(formState);
+  //   try {
+  //     const { data } = await login({
+  //       variables: { ...formState },
+  //     });
 
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-    }
+  //     Auth.login(data.login.token);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
 
     // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  };
-
+    // setFormState({
+    //   email: '',
+    //   password: '',
+    // });
+  // };
+ 
   return (
-    <main className="flex-row justify-center mb-4">
+    <>
+    <CssBaseline />
+    <Container disableGutters maxWidth="false" sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+      
+
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <h1>Something</h1>
+          
+        </Grid>
+        <Grid item xs={4}>
+          
+          {/* <form onSubmit={handleFormSubmit}> */}
+          <form>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  // value={formState.email}
+                  // onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  // value={formState.password}
+                  // onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+
+          
+        </Grid>
+      </Grid>
+
+
+      {/*  */}
+    {/* <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Login</h4>
@@ -51,8 +103,8 @@ const Login = (props) => {
                 Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
+            ) : ( */}
+              {/* <form onSubmit={handleFormSubmit}>
                 <input
                   className="form-input"
                   placeholder="Your email"
@@ -74,10 +126,10 @@ const Login = (props) => {
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
-                  Submit
+                  Test Button
                 </button>
-              </form>
-            )}
+              </form> */}
+            {/* )}
 
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
@@ -87,8 +139,15 @@ const Login = (props) => {
           </div>
         </div>
       </div>
-    </main>
+    </main> */}
+{/*  */}
+
+
+
+    </Container>
+    </>
   );
-};
+}
+;
 
 export default Login;
