7d5acdaf44cf7169f6d0bf72e104f1361867613e
Christina Bohn <christinajbohn@gmail.com>
2022-04-15 23:22:38 -0700

    Updated mutations to match typeDefs.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,28 +25,26 @@ export const ADD_USER = gql`
   }
 `;
 
-// TODO: Double check arguemnts, add to rest of mutations
 // export const LOGIN_USER = gql`
 //   mutation login(
 //     $username: String!,
-//     # $email: String, 
+//     $email: String, 
 //     $password: String!,
-//     # $firstName: String,
-//     # $lastName: String
+//     $firstName: String,
+//     $lastName: String
 //     ) {
 //     login(
 //       username: $username,
-//       # email: $email, 
+//       email: $email, 
 //       password: $password,
-//       # firstName: $firstName,
-//       # lastName: $lastName
+//       firstName: $firstName,
+//       lastName: $lastName
 //       ) {
 //       token
 //       user {
 //         _id
 //         username
 //         email
-//         password
 //         firstName
 //         lastName
 //       }
@@ -64,7 +62,6 @@ export const ADD_USER = gql`
 //         _id
 //         username
 //         email
-//         password
 //         firstName
 //         lastName
 //       }
@@ -72,97 +69,195 @@ export const ADD_USER = gql`
 //   }
 // `;
 
-// export const ADD_LEAGUE = gql`
-//   mutation addLeague() {
-//     addLeague() {
-//       league {
-//         _id
-//         leagueName
-//         sport
-//         leaguePic
-//         seasons
-//       }
-//     }
-//   }
-// `;
+export const ADD_LEAGUE = gql`
+  mutation addLeague(
+    $leagueName: String!, 
+    $sport: String!,
+    $leaguePic: String,
+    $seasons: [Season]
+    ) {
+    addLeague(
+      leagueName: $leagueName,
+      sport: $sport,
+      leaguePic: $leaguePic
+      seasons: $seasons
+    ) {
+      league {
+        _id
+        leagueName
+        sport
+        leaguePic
+        seasons
+      }
+    }
+  }
+`;
 
-// export const ADD_SEASON = gql`
-//   mutation addSeason() {
-//     addSeason() {
-//       season {
-//       _id
-//       startYear
-//       endYear
-//       league
-//       teams
-//       }
-//     }
-// `;
+// *** Are we keeping start/end month in season? -CJB ***
+export const ADD_SEASON = gql`
+  mutation addSeason(
+    $startYear: Int!,
+    $startMonth: Int,
+    $endYear: Int,
+    $endMonth: Int,
+    $league: [League],
+    $teams: [Team],
+  ) {
+    addSeason(
+      startYear: $startYear,
+      startMonth: $startMonth,
+      endYear: $endYear,
+      endMonth: $endMonth,
+      league: $league,
+      teams: $teams,
+    ) {
+      season {
+      _id
+      startYear
+      endYear
+      league
+      teams
+      }
+    }
+  }
+`;
 
-// export const ADD_SOCCERTEAM = gql`
-//   mutation addTeam() {
-//     addTeam() {
-//       soccerTeam {
-//       _id
-//         teamName
-//         teamColor
-//         teamPic
-//         league
-//         seasons
-//         games
-//         roster
-//         wins
-//         draws
-//         losses
-//         goalsFor
-//         goalsAgainst
-//         goalDifferential
-//       }
-//     }
-//   }
-// `;
+export const ADD_SOCCERTEAM = gql`
+  mutation addTeam(
+    $teamName: String!,
+    $teamColor: String,
+    $teamPic: String,
+    $league: [League],
+    $seasons: [Season],
+    $games: [SoccerGame],
+    $roster: [SoccerPlayer],
+    $wins: Int,
+    $draws: Int,
+    $losses: Int,
+    $goalsFor: Int,
+    $goalsAgainst: Int,
+    $goalDifferential: Int,
+  ) {
+    addTeam(
+      teamName: $teamName,
+      teamColor: $teamColor,
+      teamPic: $teamPic,
+      league: $league,
+      seasons: $seasons,
+      games: $games,
+      roster: $roster,
+      wins: $wins,
+      draws: $draws,
+      losses: $losses
+      goalsFor: $goalsFor,
+      goalsAgainst: $goalsAgainst,
+      goalDifferential: $goalDifferential
+    ) {
+      soccerTeam {
+        _id
+        teamName
+        teamColor
+        teamPic
+        league
+        seasons
+        games
+        roster
+        wins
+        draws
+        losses
+        goalsFor
+        goalsAgainst
+        goalDifferential
+      }
+    }
+  }
+`;
 
-// export const ADD_SOCCERPLAYER = gql`
-//   mutation addPlayer() {
-//     addPlayer() {
-//       soccerPlayer {
-//         _id
-//         playerFirstName
-//         playerLastName
-//         playerPic
-//         playerNumber
-//         # Correct syntax?
-//         goals [
-//           gameDate
-//           minute
-//           totalGoals
-//         ]
-//         assists [
-//           gameDate
-//           minute
-//           totalAssists
-//         ]
-//         teams
-//       }
-//     }
-//   }
-// `;
+export const ADD_SOCCERPLAYER = gql`
+  mutation addPlayer(
+    $playerFirstName: String!,
+    $playerLastName: String!,
+    $playerPic: String,
+    $playerNumber: Int,
+    $goals: [Goal],
+    $assists: [Assist],
+    $teams: [Team]
+  ) {
+    addPlayer(
+      playerFirstName: $playerFirstName,
+      playerLastName: $playerLastName,
+      playerPic: $playerPic,
+      playerNumber: $playerNumber,
+      goals: $goals,
+      assists: $assists,
+      teams: $teams
+    ) {
+      soccerPlayer {
+        _id
+        playerFirstName
+        playerLastName
+        playerPic
+        playerNumber
+        teams
+      }
+    }
+  }
+`;
 
 
-// export const ADD_SOCCERGAME = gql`
-//   mutation addGame() {
-//     addGame() {
-//       soccerGame {
-//         _id
-//         gameDate
-//         homeTeam
-//         awayTeam
-//         goalsHome
-//         goalsAway
-//         assistsHome
-//         assistsAway
-//       }
-//     }
-//   }
-// `;
+export const ADD_SOCCERGAME = gql`
+  mutation addGame(
+    $gameDate: String!,
+    $homeTeam: [Team],
+    $awayTeam: [Team],
+    $goalsHome: Int,
+    $goalsAway: Int,
+    $assistsHome: Int,
+    $assistsAway: Int,
+  ) {
+    addGame(
+      gameDate: $gameDate,
+      homeTeam: $homeTeam,
+      awayTeam: $awayTeam,
+      goalsHome: $goalsHome,
+      goalsAway: $goalsAway,
+      assistsHome: $assistsHome,
+      assistsAway: $assistsAway
+    ) {
+      soccerGame {
+        _id
+        gameDate
+        homeTeam
+        awayTeam
+        goalsHome
+        goalsAway
+        assistsHome
+        assistsAway
+      }
+    }
+  }
+`;
 
+export const ADD_GOAL = gql`
+  mutation addGoal($game: SoccerGame, $minute: Int) {
+    addGoal(game: $game, minute: $minute) {
+      goal {
+        _id
+        game
+        minute
+      }
+    }
+  }
+`;
+
+export const ADD_ASSIST = gql`
+  mutation addAssist($game: SoccerGame, $minute: Int) {
+    addAssist(game: $game, minute: $minute) {
+      assist {
+        _id
+        game
+        minute
+      }
+    }
+  }
+`;
