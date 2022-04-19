4013dbd8fd32c9f7b8fb10b3656036f3615aac49
Chandra Holt <chandra_holt@hotmail.com>
2022-04-18 19:15:49 -0700

    Added custom footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,8 +1,40 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import AddModeratorIcon from "@mui/icons-material/AddModerator";
+import HomeIcon from "@mui/icons-material/Home";
+
 const Footer = () => {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      Future Footer
-    </footer>
+    <Container
+      xs={12}
+      sx={{ width: 500, position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Homepage" href="/" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="Admin Login"
+          href="login"
+          icon={<AdminPanelSettingsIcon />}
+        />
+        <BottomNavigationAction
+          label="Admin Signup"
+          href="signup"
+          icon={<AddModeratorIcon />}
+        />
+      </BottomNavigation>
+    </Container>
   );
 };
 
