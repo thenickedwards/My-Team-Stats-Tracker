2c781eef6c9b2fe731a1efd7796d54cdd603eabd
Chandra Holt <chandra_holt@hotmail.com>
2022-04-20 19:33:11 -0700

    Added localStorage.js to access stored data

--- client/src/pages/Leagues.js
+++ client/src/pages/Leagues.js
@@ -1,143 +1,146 @@
-import * as React from 'react';
+import * as React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_LEAGUES } from "../utils/queries";
+import Auth from "../utils/auth";
+import { leagueIds } from "../utils/localStorage";
 
 // Material UI Imports
-import { 
-    Box,
-    Container,
-    Grid,
-    IconButton,
-    Paper,
-    Typography
-} from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { textAlign } from '@mui/system';
-
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 // Styles
 const leaguesStyle = {
-    leaguePaper: {
-        display: 'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
-        borderRadius: 4,
-        height: "200px",
-        margin: "auto",
-        pt: 3
-    },
-    leaguePaperText: {
-        width: "70%",
-        margin: "auto",
-        textAlign: "center"
-    }
-  }
+  leaguePaper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: 4,
+    height: "200px",
+    margin: "auto",
+    pt: 3,
+  },
+  leaguePaperText: {
+    width: "70%",
+    margin: "auto",
+    textAlign: "center",
+  },
+};
 
 export default function Leagues() {
-    
-    // Functionality for Add League Modal
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    
-    return (
-      <Container  alignItems="center" justifyContent="center">
+  // Functionality for Add League Modal
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
 
-        <Grid container sx={{py: 8, px: 5}} position="relative">
+  // Render league cards
+  const [findLeagues] = useQuery(QUERY_LEAGUES);
+  const renderLeagues = async (event) => {
+    event.preventDefault();
+    if (!leagues) {
+      return false;
+    }
 
-            {/* Header and "Add" button */}
-            <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '20px', mb: 5}}>
+    try {
+      const response = await findLeagues();
 
-                <Typography variant="h1">
-                    Leagues
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+
+      const { items } = await response.json();
+
+      const leagues = items.map((league) => ({
+        leagueId: league._id,
+        leagueName: league.leagueName,
+        leaguePic: league.leaguePic,
+        leagueSeason: league.seasons,
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Container alignItems="center" justifyContent="center">
+      <Grid container sx={{ py: 8, px: 5 }} position="relative">
+        {/* Header and "Add" button */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "20px",
+            mb: 5,
+          }}
+        >
+          <Typography variant="h1">Leagues</Typography>
+
+          <IconButton
+            onClick={handleOpen}
+            aria-label="Add League"
+            size="medium"
+            sx={{
+              backgroundColor: "secondary.accent",
+              borderRadius: 10,
+              "&:hover": {
+                backgroundColor: "primary.main",
+              },
+            }}
+          >
+            <AddIcon fontSize="inherit" sx={{ color: "#ffffff" }} />
+          </IconButton>
+        </Box>
+
+        {/* League Cards - Map Over Seeds */}
+
+        <Grid container sx={{ justifyContent: "space-around" }}>
+          {leagues.map((league) => (
+            <Grid key={league.id}>
+              <Paper elevation={5} sx={leaguesStyle.leaguePaper}>
+                <img
+                  src={league.leaguePic}
+                  alt="logo"
+                  loading="lazy"
+                  height={100}
+                />
+                <Typography
+                  variant="p"
+                  gutterBottom
+                  component="div"
+                  sx={leaguesStyle.leaguePaperText}
+                >
+                  {league.leagueName}
                 </Typography>
-
-                <IconButton onClick={handleOpen} aria-label="Add League" size="medium"
-                    sx={{
-                        backgroundColor: 'secondary.accent',
-                        borderRadius: 10,
-                        '&:hover': {
-                            backgroundColor: 'primary.main'
-                        }
-                    }}>
-                    <AddIcon fontSize="inherit" sx={{color: '#ffffff'}}/>
-                </IconButton>
-
-            </Box>
-            
-            {/* League Cards - Map Over Seeds */}
-            <Grid container  sx={{justifyContent: 'space-around'}}>
-                <Grid >
-
-                    <Paper elevation={5} sx={leaguesStyle.leaguePaper} >
-                        <img
-                        src="images/washington-premier-league.png"
-                        alt="logo"
-                        loading="lazy"
-                        height={100}
-                        />
-                        <Typography variant="p" gutterBottom component="div" sx={leaguesStyle.leaguePaperText}>
-                            Washington Premier League
-                        </Typography>
-                    </Paper>
-
-                </Grid>
-                <Grid>
-
-                    <Paper elevation={5} sx={leaguesStyle.leaguePaper}>
-                        <img
-                        src="images/WSA.png"
-                        alt="logo"
-                        loading="lazy"
-                        height={100}
-                        />
-                        <Typography variant="p" gutterBottom component="div" sx={leaguesStyle.leaguePaperText}>
-                            Washington Soccer Academy
-                        </Typography>
-                    </Paper>
-
-                </Grid>
-                <Grid>
-
-                    <Paper elevation={5} sx={leaguesStyle.leaguePaper}>
-                        <img
-                        src="images/chicago.png"
-                        alt="logo"
-                        loading="lazy"
-                        height={100}
-                        />
-                        <Typography variant="p" gutterBottom component="div" sx={leaguesStyle.leaguePaperText}>
-                            Washington Premier League
-                        </Typography>
-                    </Paper>
-
-                </Grid>
-                <Grid>
-
-                    <Paper elevation={5} sx={leaguesStyle.leaguePaper}>
-                        <img
-                        src="images/chicago.png"
-                        alt="logo"
-                        loading="lazy"
-                        height={100}
-                        />
-                        <Typography variant="p" gutterBottom component="div" sx={leaguesStyle.leaguePaperText}>
-                            Washington Premier League
-                        </Typography>
-                    </Paper>
-
-                </Grid>
+              </Paper>
             </Grid>
-
+          ))}
         </Grid>
+      </Grid>
 
-        {/* Page styling */}
-        <Box sx={{position: 'absolute', top: 100, right: 15}}>
-            <img src='images/abstract-up-arrows.png' alt="Abstract graphic with arrows." width='60px'/>
-        </Box>
-        
-        <Box sx={{position: 'absolute', bottom: 40, left: 40}}>
-            <img src='images/abstract-corner-dots-lines.png' alt="Abstract graphic with dots and lines." width='250px'/>
-        </Box>
+      {/* Page styling */}
+      <Box sx={{ position: "absolute", top: 100, right: 15 }}>
+        <img
+          src="images/abstract-up-arrows.png"
+          alt="Abstract graphic with arrows."
+          width="60px"
+        />
+      </Box>
 
-      </Container>
-    );
-  }
+      <Box sx={{ position: "absolute", bottom: 40, left: 40 }}>
+        <img
+          src="images/abstract-corner-dots-lines.png"
+          alt="Abstract graphic with dots and lines."
+          width="250px"
+        />
+      </Box>
+    </Container>
+  );
+}
