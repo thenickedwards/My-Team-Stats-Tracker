f7b54fc559a7161154ea30824f83661ff3c512e3
Juanita Samborski <juanita@breakthroughwebsite.com>
2022-04-21 13:12:10 -0700

    Completed Add Team form

--- client/src/pages/Teams.js
+++ client/src/pages/Teams.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
+import AddTeam from "../components/Forms/AddTeam";
 
 // Material UI Imports
 import { 
     Box,
-    Button,
     Container,
     FormControl,
     Grid,
@@ -13,12 +13,9 @@ import {
     Modal,
     Paper,
     Select,
-    TextField,
     Typography,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-// import { textAlign } from '@mui/system';
-
 
 // Styles
 const teamsStyle = {
@@ -46,27 +43,15 @@ const teamsStyle = {
         borderRadius: 3,
         boxShadow: 24,
         p: 4,
-    },
-    formButton: { 
-        height: 50, 
-        backgroundColor: "secondary.main",
-        "&:hover": {
-          backgroundColor: "primary.main",
-        },
-    },
+    }
   }
 
 export default function Teams() {
     
-    // Functionality for Dropdowns
+    // // Functionality for Dropdowns
     const [league, setLeague] = React.useState('');
     const handleLeagueChange = (event) => {
         setLeague(event.target.value);
-    };
-    
-    const [season, setSeason] = React.useState('');
-    const handleSeasonChange = (event) => {
-        setSeason(event.target.value);
     };
     
     // Functionality for Add Team Modal
@@ -77,8 +62,7 @@ export default function Teams() {
     return (
       <Container  alignItems="center" justifyContent="center">
 
-
-        {/* Page styling */}
+        {/* Abstract Images */}
         <Box sx={{position: 'absolute', top: 100, right: 15}}>
             <img src='images/abstract-up-arrows.png' alt="Abstract graphic with arrows." width='60px'/>
         </Box>
@@ -87,85 +71,76 @@ export default function Teams() {
             <img src='images/abstract-corner-dots-lines.png' alt="Abstract graphic with dots and lines." width='250px'/>
         </Box>
 
+
         <Grid container sx={{py: 8, px: 5}} position="relative">
-
-            {/* Header and "Add" button */}
-            <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '20px', mb: 5}}>
-
-                <Typography variant="h1">
+     
+        {/* Container for the two top columns. */}
+          <Grid container alignItems={"center"}>
+            {/* PAGE HEADING. Left column. */}
+            <Grid item xs={12} sm={12} md={9} lg={9}>
+              <Grid container sx={{ display: "flex", flexDirection: "column" }}>
+                <Grid
+                  container
+                  item
+                  direction={{ xs: "row" }}
+                  alignItems={{xs: "start"}}
+                  sx={{ gap: "20px", mb: 5 }}
+                >
+                  <Typography variant="h1" color="secondary.contrastText">
                     Teams
-                </Typography>
+                  </Typography>
 
-                <IconButton onClick={handleOpen} aria-label="Add Team" size="medium"
+                  <IconButton
+                    onClick={handleOpen}
+                    aria-label="Add Game"
+                    size="medium"
                     sx={{
-                        backgroundColor: 'secondary.accent',
-                        borderRadius: 10,
-                        '&:hover': {
-                            backgroundColor: 'primary.main'
-                        }
-                    }}>
-                    <AddIcon fontSize="inherit" sx={{color: '#ffffff'}}/>
-                </IconButton>
-
-            </Box>
+                      backgroundColor: "secondary.accent",
+                      borderRadius: 10,
+                      "&:hover": {
+                        backgroundColor: "primary.main",
+                      },
+                    }}
+                  >
+                    <AddIcon fontSize="inherit" sx={{ color: "#ffffff" }} />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </Grid>
             
             {/* Dropdown. League Selector. */}
-            <FormControl size="small" 
-                sx={{ 
-                    m: 1, 
-                    minWidth: '100%', 
-                    mb: 5, 
-                    ml: 0,
-                    '& .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'secondary.main'
-                    }
+            <Grid item xs={12} sm={12} md={3} lg={3}>
+              <FormControl
+                size="small"
+                sx={{
+                  m: 1,
+                  minWidth: "100%",
+                  mb: 5,
+                  ml: 0,
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "secondary.main",
+                  },
                 }}
-                >
+              >
                 <InputLabel id="select-league">League</InputLabel>
                 <Select
-                    labelId="select-league"
-                    id="select-league"
-                    value={league}
-                    label="League"
-                    onChange={handleLeagueChange}
+                  labelId="select-league"
+                  id="select-league"
+                  value={league}
+                  label="League"
+                  onChange={handleLeagueChange}
                 >
-                    <MenuItem value="">
+                  <MenuItem value="">
                     <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
                 </Select>
-            </FormControl>
+              </FormControl>
+            </Grid>
+          </Grid>
 
-            {/* Dropdown. Season Selector. */}
-            <FormControl size="small" 
-                sx={{ 
-                    m: 1, 
-                    minWidth: '100%', 
-                    mb: 5, 
-                    ml: 0,
-                    '& .MuiOutlinedInput-notchedOutline': {
-                        borderColor: 'secondary.main'
-                    }
-                }}
-                >
-                <InputLabel id="select-season">Season</InputLabel>
-                <Select
-                    labelId="select-season"
-                    id="select-season"
-                    value={season}
-                    label="Season"
-                    onChange={handleSeasonChange}
-                >
-                    <MenuItem value="">
-                    <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
 
             {/* Team Cards - Map Over Seeds */}
             <Grid container  spacing={{xs: 4}}>
@@ -236,7 +211,6 @@ export default function Teams() {
             </Grid>
 
             {/* Add Team Modal */}
-
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -248,44 +222,10 @@ export default function Teams() {
                         Add Team
                     </Typography>
 
+
+                    {/* ADD TEAM FORM */}
+                    <AddTeam handleClose={handleClose} />
                 
-                    <FormControl fullWidth sx={{gap:4}}>
-
-                        <TextField id="teamName" label="Team Name" variant="outlined" color="secondary"
-                        InputLabelProps={{ shrink: true }} 
-                        />
-                        
-                        {/* TODO: Add color picker? */}
-                        <TextField id="teamColor" label="Team Color" variant="outlined" color="secondary"
-                        InputLabelProps={{ shrink: true }} 
-                        />
-
-                        <TextField id="season" label="Season" variant="outlined" color="secondary"
-                        InputLabelProps={{ shrink: true }} 
-                        />
-
-                        <TextField id="games" label="Games" variant="outlined" color="secondary"
-                        InputLabelProps={{ shrink: true }} 
-                        /> 
-
-                        <TextField id="roster" label="Roster" variant="outlined" color="secondary"
-                        InputLabelProps={{ shrink: true }} 
-                        />    
-
-                        {/* TODO: Add Upload Photo Field */}
-
-                        <Button
-                        variant="contained"
-                        type="submit"
-                        sx={teamsStyle.formButton}
-                        fullWidth
-                        disableElevation
-                        >
-                            <Typography variant="h3">Add Team</Typography>
-
-                        </Button>
-
-                    </FormControl>
 
                 </Box>
             </Modal>
